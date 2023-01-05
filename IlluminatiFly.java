df7313eebaaf3696fe57379f0d8c45d4558923a1
ToxicCoding <116459073+ToxicAngelX@users.noreply.github.com>
2023-01-05 22:11:06 +0300

    Update IlluminatiFly.java

--- IlluminatiFly.java
+++ IlluminatiFly.java
@@ -3,17 +3,17 @@ package me.illuminatiproductions.illuminatifly;
 import me.illuminatiproductions.illuminatifly.commands.Fly;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class IlluminatiFly extends JavaPlugin {
+public final class ToxicFly extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        System.out.println("[IlluminatiFly] Plugin now online.");
+        // Plugin startup logic <dont delete it >
+        System.out.println("[ToxicFly] Plugin now online.");
 
-        //Register command
+        //Register command < dont edit it if you dont know what do you do >
         getCommand("fly").setExecutor(new Fly(this));
 
-        //Config
+        //Config < dont edit it if you dont know what do you do >
         getConfig().options().copyDefaults();
         saveDefaultConfig();
     }
